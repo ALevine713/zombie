118db7aae8609f774c87b00c384706064e1ba38a
PradeepLR25 <PradeepLR25@gmail.com>
2017-05-12 10:57:51 -0700

    Initial Commit

---
+++ ZobmbieGameTests/ZobmbieGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZobmbieGameTests.swift
+//  ZobmbieGameTests
+//
+//  Created by Pradeep on 5/12/17.
+//  Copyright © 2017 Pradeep. All rights reserved.
+//
+
+import XCTest
+@testable import ZobmbieGame
+
+class ZobmbieGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
